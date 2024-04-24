585361091be3f59175fc5c43f583d9f1a1d1a4cb
DZC31VG <hasanciftcii26@gmail.com>
2024-04-24 10:17:34 +0300

    .d.ts.map remove from generated type declarations

--- correct-types.js
+++ correct-types.js
@@ -12,3 +12,25 @@ fs.readFile(path.join("dist", "index.d.ts"), "utf8", function (err, data) {
         if (err) return console.log(err);
     });
 });
+
+const generatedFilePath = path.join(__dirname, "dist", "resources", "ui5", "antares");
+
+fs.promises.readdir(generatedFilePath, { recursive: true }).then((files) => {
+    files.forEach((file) => {
+        if (file.endsWith(".d.ts")) {
+            const filePath = path.join(generatedFilePath, file);
+
+            fs.readFile(filePath, "utf8", function (err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                const result = data.replace(/\/\/# sourceMappingURL\=[A-Za-z]*\.d\.ts\.map/, "");
+
+                fs.writeFile(filePath, result, "utf8", function (err) {
+                    if (err) return console.log(err);
+                });
+            });
+        }
+    });
+});
