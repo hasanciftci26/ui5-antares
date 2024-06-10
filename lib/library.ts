8111a50e29447992a17f495dc82c977802bad854
DZC31VG <hasanciftcii26@gmail.com>
2024-06-10 22:40:57 +0200

    1.108.30002 release for ValidationLogicCL value1 and value2 bug fixes, caseSensitive param for ValueHelpCL

--- lib/library.ts
+++ lib/library.ts
@@ -34,7 +34,7 @@ sap.ui.getCore().initLibrary({
         "ui5.antares.util.Util"
     ],
     noLibraryCSS: true,
-    version: "1.108.30001"
+    version: "1.108.30002"
 });
 
 const library: { [key: string]: unknown } = ObjectPath.get("ui5.antares") as { [key: string]: unknown };
